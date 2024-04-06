fd0194370d36c1b8629e454be23607cbee7a77ed
Shareef0226 <142314145+Shareef0226@users.noreply.github.com>
2024-04-06 21:37:08 +0530

    0226 Update variables.tf

--- azure/key_vault/variables.tf
+++ azure/key_vault/variables.tf
@@ -18,22 +18,7 @@ variable "sku_name" {
   type = string
 }
 
-variable "enable_rbac_authorization" {
-  type    = bool
-  default = false
-}
-
-variable "enabled_for_deployment" {
-  type    = bool
-  default = true
-}
-
 variable "enabled_for_disk_encryption" {
-  type    = bool
-  default = true
-}
-
-variable "enabled_for_template_deployment" {
   type    = bool
   default = true
 }
@@ -43,10 +28,6 @@ variable "purge_protection_enabled" {
   default = true
 }
 
-variable "tags" {
-  type = map(string)
-}
-
 variable "soft_delete_retention_days" {
   type = number
   default = 90
@@ -54,13 +35,4 @@ variable "soft_delete_retention_days" {
 
 variable "terraform_identity_object_id" {
   type    = string
-}
-
-variable "network_acls" {
-  type = object({
-    bypass                      = string
-    default_action              = string
-    ip_rules                    = string
-    virtual_network_subnet_ids  = string
-  })
 }
