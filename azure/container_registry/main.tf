650148e1c62b4c64470536a10e6340aacfce4069
Shareef0226 <142314145+Shareef0226@users.noreply.github.com>
2024-04-06 21:29:10 +0530

    0226 Update main.tf

--- azure/container_registry/main.tf
+++ azure/container_registry/main.tf
@@ -7,7 +7,6 @@ resource "azurerm_container_registry" "current" {
     location                      = var.location
     name                          = var.name
     resource_group_name           = var.resource_group_name
-    tags                          = var.tags
     sku                           = var.sku
     admin_enabled                 = var.admin_enabled
     zone_redundancy_enabled       = var.zone_redundancy_enabled
