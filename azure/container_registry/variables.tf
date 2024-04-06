2c2a724741cbd51065595efe9fd45b453be103e9
Shareef0226 <142314145+Shareef0226@users.noreply.github.com>
2024-04-06 21:28:40 +0530

    0226 Update variables.tf

--- azure/container_registry/variables.tf
+++ azure/container_registry/variables.tf
@@ -15,38 +15,9 @@ variable "sku" {
     default = "Standard"
 }
 
-variable "tags" {
-    type    = map(string)
-}
-
-variable "network_rule_bypass_option" {
-    type    = string
-    default = "AzureServices"
-}
-
 variable "admin_enabled" {
     type    = bool
     default = true
-}
-
-variable "anonymous_pull_enabled" {
-    type    = bool
-    default = false
-}
-
-variable "data_endpoint_enabled" {
-    type    = bool
-    default = false
-}
-
-variable "public_network_access_enabled" {
-    type    = bool
-    default = true
-}
-
-variable "quarantine_policy_enabled" {
-    type    = bool
-    default = false
 }
 
 variable "zone_redundancy_enabled" {
